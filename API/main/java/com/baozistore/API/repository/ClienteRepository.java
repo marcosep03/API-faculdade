487d3228c140c86c0510e759383b954a3defec90
Marcos Eduardo <4758937@alunouninter.com>
2026-08-24 21:39:44 -0300

    Delete API directory
    API ERRADA DNV

--- API/main/java/com/baozistore/API/repository/ClienteRepository.java
+++
@@ -1,6 +0,0 @@
-// ClienteRepository.java
-package com.baozistore.API.repository;
-import com.baozistore.API.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {}
--- API/main/java/com/baozistore/API/repository/ProdutoRepository.java
+++
@@ -1,6 +0,0 @@
-// ProdutoRepository.java
-package com.baozistore.API.repository;
-import com.baozistore.API.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {}
