9f1ce1f12ac963d99103db34495299ec0420d641
Marcos Eduardo <4758937@alunouninter.com>
2026-08-24 21:35:53 -0300

    Delete API directory
    Enviei errado

--- API/main/java/com/baozistore/API/repository/PedidoRepository.java
+++
@@ -1,6 +0,0 @@
-// PedidoRepository.java
-package com.baozistore.API.repository;
-import com.baozistore.API.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {}
